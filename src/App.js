6b05c342c11dc62216b92c71f53dd42e8c51f2d1
Erico Almeida <ericoalmeidamvp@gmail.com>
2020-04-05 11:39:32 -0400

    Feature: Global style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React from 'react';
+import GlobalStyle from './styles/Global';
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <h1>Hello world</h1>
+    </div>
+  );
 }
 
 export default App;
